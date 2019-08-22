a2821f555b997282b9dd2ffb78b07d20bdbf1526
CezaryZal <czarek0821@gmail.com>
2019-08-22 16:37:23 +0200

    chang version in jackson-databind dependence and add AppInitializer class

--- src/main/java/spring/config/SpringMvcDispatcherServletInitializer.java
+++ src/main/java/spring/config/SpringMvcDispatcherServletInitializer.java
@@ -1,20 +1,20 @@
-package spring.config;
-
-import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
-
-public class SpringMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
-    @Override
-    protected Class<?>[] getRootConfigClasses() {
-        return null;
-    }
-
-    @Override
-    protected Class<?>[] getServletConfigClasses() {
-        return new Class[] { AppConfig.class };
-    }
-
-    @Override
-    protected String[] getServletMappings() {
-        return new String[] {"/"};
-    }
-}
+//package spring.config;
+//
+//import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+//
+//public class SpringMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+//    @Override
+//    protected Class<?>[] getRootConfigClasses() {
+//        return null;
+//    }
+//
+//    @Override
+//    protected Class<?>[] getServletConfigClasses() {
+//        return new Class[] { AppConfig.class };
+//    }
+//
+//    @Override
+//    protected String[] getServletMappings() {
+//        return new String[] {"/"};
+//    }
+//}
---
+++ src/main/web/WEB-INF/index.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: czar
+  Date: 22/08/2019
+  Time: 11:40
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Spring REST</title>
+</head>
+<body>
+Spring REST
+<hr>
+</body>
+</html>
